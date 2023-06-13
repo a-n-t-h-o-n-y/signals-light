d522422f3862856a55edc01c141d0d364b248205
Anthony Leedom <anthonym.leedom@gmail.com>
2023-06-13 13:42:07 -0700

    Update tests

--- tests/signal.test.cpp
+++ tests/signal.test.cpp
@@ -1,133 +1,200 @@
-#include <memory>
 #include <optional>
 #include <stdexcept>
 #include <type_traits>
 
-#include <catch2/catch.hpp>
+#include <catch2/catch_test_macros.hpp>
 
 #include <signals_light/signal.hpp>
-#include <type_traits>
 
-TEST_CASE("Empty Signal returns std::nullopt or void", "[Signal]")
+TEST_CASE("Signal with no Slots", "[Signal]")
 {
-    sl::Signal<int()> si;
-    sl::Signal<void()> sv;
-    CHECK(si() == std::nullopt);
-    static_assert(std::is_same_v<decltype(sv()), void>);
+    SECTION("void Signal return type will return void")
+    {
+        {
+            auto sig = sl::Signal<void()>{};
+            REQUIRE(std::is_same_v<decltype(sig()), void>);
+        }
+        {
+            auto sig = sl::Signal<void(int, char, float)>{};
+            REQUIRE(std::is_same_v<decltype(sig(1, 'a', 3.14f)), void>);
+        }
+    }
+    SECTION("non-void Signal return type will return std::nullopt")
+    {
+        {
+            auto sig = sl::Signal<int()>{};
+            REQUIRE(std::is_same_v<decltype(sig()), std::optional<int>>);
+            REQUIRE(sig() == std::nullopt);
+        }
+        {
+            auto sig = sl::Signal<int(int, char, float)>{};
+            REQUIRE(std::is_same_v<decltype(sig(1, 'a', 3.14f)),
+                                   std::optional<int>>);
+            REQUIRE(sig(1, 'a', 3.14f) == std::nullopt);
+        }
+    }
 }
 
-TEST_CASE("Single Slot connect and disconnect", "[Signal]")
+TEST_CASE("Slot connect and disconnectd", "[Signal]")
 {
-    sl::Signal<int()> s;
-    sl::Identifier id = s.connect([] { return 5; });
-    CHECK(!s.is_empty());
-    CHECK(s.slot_count() == 1);
+    auto sig = sl::Signal<int()>{};
 
-    std::optional<int> result = s();
-    REQUIRE(result.has_value());
-    CHECK(*result == 5);
-    s.disconnect(id);
-    CHECK(s.is_empty());
-    CHECK(s.slot_count() == 0);
-    CHECK(s() == std::nullopt);
+    SECTION(
+        "Returns the value of the connected Slot when only one Slot connected")
+    {
+        REQUIRE(sig.is_empty());
+        REQUIRE(sig.slot_count() == 0);
+        REQUIRE(sig() == std::nullopt);
+
+        auto const id = sig.connect([] { return 5; });
+        REQUIRE(std::is_same_v<decltype(id), sl::Identifier const>);
+
+        REQUIRE(!sig.is_empty());
+        REQUIRE(sig.slot_count() == 1);
+
+        auto const result = sig();
+        REQUIRE(std::is_same_v<decltype(result), std::optional<int> const>);
+        REQUIRE(result.has_value());
+        REQUIRE(*result == 5);
+
+        sig.disconnect(id);
+        REQUIRE(sig.is_empty());
+        REQUIRE(sig.slot_count() == 0);
+        REQUIRE(sig() == std::nullopt);
+    }
+
+    SECTION(
+        "Returns the return value of the last connected Slot when multiple "
+        "Slots connected")
+    {
+        REQUIRE(sig.is_empty());
+        REQUIRE(sig.slot_count() == 0);
+        REQUIRE(sig() == std::nullopt);
+
+        auto const id_1 = sig.connect([] { return 5; });
+        REQUIRE(std::is_same_v<decltype(id_1), sl::Identifier const>);
+
+        REQUIRE(!sig.is_empty());
+        REQUIRE(sig.slot_count() == 1);
+
+        auto const id_2 = sig.connect([] { return 3; });
+        REQUIRE(std::is_same_v<decltype(id_2), sl::Identifier const>);
+
+        REQUIRE(!sig.is_empty());
+        REQUIRE(sig.slot_count() == 2);
+
+        auto const result = sig();
+        REQUIRE(std::is_same_v<decltype(result), std::optional<int> const>);
+        REQUIRE(result.has_value());
+        REQUIRE(*result == 3);
+
+        SECTION("Disconnecting Slot 1 will still return Slot 2's return value.")
+        {
+            sig.disconnect(id_1);
+            REQUIRE(!sig.is_empty());
+            REQUIRE(sig.slot_count() == 1);
+
+            auto const result_2 = sig();
+            REQUIRE(
+                std::is_same_v<decltype(result_2), std::optional<int> const>);
+            REQUIRE(result_2.has_value());
+            REQUIRE(*result_2 == 3);
+        }
+
+        SECTION("Disconnecting Slot 2 will return Slot 1's return value.")
+        {
+            sig.disconnect(id_2);
+            REQUIRE(!sig.is_empty());
+            REQUIRE(sig.slot_count() == 1);
+
+            auto const result_2 = sig();
+            REQUIRE(
+                std::is_same_v<decltype(result_2), std::optional<int> const>);
+            REQUIRE(result_2.has_value());
+            REQUIRE(*result_2 == 5);
+        }
+    }
 }
 
-TEST_CASE("Multiple Slots connect and disconnect", "[Signal]")
+TEST_CASE("Emitting a Signal actually invokes it's Slot functions.", "[Signal]")
 {
-    sl::Signal<int(char, int, bool)> s;
-    sl::Identifier sum_id =
-        s.connect([](char c, int i, bool b) { return c + i + b; });
-    sl::Identifier product_id =
-        s.connect([](char c, int i, bool b) { return c * i * b; });
-    sl::Identifier difference_id =
-        s.connect([](char c, int i, bool b) { return c - i - b; });
+    auto sig = sl::Signal<int(int, int, int)>{};
 
-    CHECK(s.slot_count() == 3);
-    CHECK(*s(5, 1, false) == 4);
+    sig.connect([](int a, int b, int c) { return a + b + c; });
+    REQUIRE(sig(5, 4, 3).value() == 12);
 
-    sl::Slot<int(char, int, bool)> difference_slot =
-        s.disconnect(difference_id);
-    CHECK(difference_slot(5, 1, false) == 4);
-    CHECK(s.slot_count() == 2);
-    CHECK(*s(4, 3, true) == 12);
-
-    sl::Slot<int(char, int, bool)> sum_slot = s.disconnect(sum_id);
-    CHECK(sum_slot(5, 5, false) == 10);
-    CHECK(s.slot_count() == 1);
-    CHECK(*s(5, 5, true) == 25);
-
-    sl::Slot<int(char, int, bool)> product_slot = s.disconnect(product_id);
-    CHECK(product_slot(5, 5, true) == 25);
-    CHECK(s.slot_count() == 0);
-    CHECK(s.is_empty());
-    CHECK(s(5, 5, true) == std::nullopt);
+    sig.connect([](int a, int b, int c) { return a * b * c; });
+    REQUIRE(sig(5, 4, 3).value() == 60);
 }
 
-TEST_CASE("void return type", "[Signal]")
+TEST_CASE("Disconnecting with an invalid Identifier will throw", "[Signal]")
 {
-    sl::Signal<void(int)> s;
-    static_assert(std::is_same_v<decltype(s(5)), void>);
-    s.connect([](int) {});
-    static_assert(std::is_same_v<decltype(s(5)), void>);
+    auto sig      = sl::Signal<void()>{};
+    auto const id = sig.connect([] {});
+    REQUIRE_NOTHROW(sig.disconnect(id));
+
+    REQUIRE_THROWS_AS(sig.disconnect(id), std::invalid_argument);
+    REQUIRE_THROWS_AS(sig.disconnect(sl::Identifier{}), std::invalid_argument);
 }
 
-TEST_CASE("Throw on disconnect of invalid Identifier", "[Signal]")
+TEST_CASE("Lifetime Tracking", "[Signal]")
 {
-    sl::Signal<void()> s;
-    auto id   = s.connect([] {});
-    auto slot = s.disconnect(id);
-    CHECK_THROWS_AS(s.disconnect(id), std::invalid_argument);
-    CHECK_THROWS_AS(s.disconnect(sl::Identifier{}), std::invalid_argument);
-}
+    SECTION(
+        "Destroying the only tracked object on a Slot will disable that Slot")
+    {
+        auto sig  = sl::Signal<int()>{};
+        auto slot = sl::Slot<int()>{[] { return 5; }};
+        {
+            auto life = sl::Lifetime{};
+            slot.track(life);
+            sig.connect(slot);
+            REQUIRE(*sig() == 5);
+        }
+        REQUIRE(sig() == std::nullopt);
+    }
 
-TEST_CASE("Lifetime tracking of single object", "[Signal]")
-{
-    sl::Signal<int()> s;
-    sl::Slot<int()> slot = [] { return 5; };
-    auto life            = std::make_unique<sl::Lifetime>();
-    slot.track(*life);
-    auto id = s.connect(slot);
-    CHECK(*s() == 5);
-    life.reset();
-    CHECK(s() == std::nullopt);
-    s.disconnect(id);
-    CHECK(s() == std::nullopt);
-}
+    SECTION(
+        "Lifetime tracking only works if track() is called before connect()")
+    {
+        auto sig  = sl::Signal<int()>{};
+        auto slot = sl::Slot<int()>{[] { return 5; }};
+        sig.connect(slot);
+        {
+            auto life = sl::Lifetime{};
+            slot.track(life);
+            REQUIRE(*sig() == 5);
+        }
+        REQUIRE(*sig() == 5);
+    }
 
-TEST_CASE("Lifetime tracking of multiple objects", "[Signal]")
-{
-    sl::Signal<int()> s;
-    sl::Slot<int()> slot_5 = [] { return 5; };
-    sl::Slot<int()> slot_3 = [] { return 3; };
+    SECTION(
+        "Only a single tracked object needs to be destroyed to disable a Slot")
+    {
+        auto sig    = sl::Signal<int()>{};
+        auto slot   = sl::Slot<int()>{[] { return 5; }};
+        auto life_1 = sl::Lifetime{};
+        slot.track(life_1);
+        {
+            auto life_2 = sl::Lifetime{};
+            slot.track(life_2);
+            sig.connect(slot);
+            REQUIRE(*sig() == 5);
+        }
+        REQUIRE(sig() == std::nullopt);
+    }
 
-    auto life_1 = std::make_unique<sl::Lifetime>();
-    auto life_2 = std::make_unique<sl::Lifetime>();
-    auto life_3 = std::make_unique<sl::Lifetime>();
-    auto life_4 = std::make_unique<sl::Lifetime>();
-    auto life_5 = std::make_unique<sl::Lifetime>();
-    auto life_6 = std::make_unique<sl::Lifetime>();
-
-    slot_5.track(*life_1);
-    slot_5.track(*life_2);
-    slot_5.track(*life_3);
-    slot_5.track(*life_4);
-    slot_3.track(*life_5);
-    slot_3.track(*life_6);
-
-    s.connect(slot_5);
-    s.connect(slot_3);
-
-    CHECK(*s() == 3);
-    life_1.reset();
-    CHECK(*s() == 3);
-    life_5.reset();
-    CHECK(s() == std::nullopt);
-    life_6.reset();
-    CHECK(s() == std::nullopt);
-    life_2.reset();
-    CHECK(s() == std::nullopt);
-    life_3.reset();
-    CHECK(s() == std::nullopt);
-    life_4.reset();
-    CHECK(s() == std::nullopt);
+    SECTION("Non-disabled Slots will still be run")
+    {
+        auto sig    = sl::Signal<int()>{};
+        auto slot_1 = sl::Slot<int()>{[] { return 5; }};
+        sig.connect(slot_1);
+        {
+            auto slot_2 = sl::Slot<int()>{[] { return 3; }};
+            auto life   = sl::Lifetime{};
+            slot_2.track(life);
+            sig.connect(slot_2);
+            REQUIRE(*sig() == 3);
+        }
+        REQUIRE(*sig() == 5);
+    }
 }
